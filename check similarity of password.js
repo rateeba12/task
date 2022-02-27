6063da0c0d795846a0d0935e4dfb79ca46a8cd95
rateeba12 <91503583+rateeba12@users.noreply.github.com>
2022-02-27 10:34:56 +0200

    Add files via upload

---
+++ check similarity of password.js
@@ -0,0 +1,10 @@
+let value1= document.getElementById("password").value;
+let value2= document.getElementById("pass").value;
+let element= document.getElementById("b");
+ element.addEventListener("click", function(){ 
+ if ( document.getElementById("password").value != document.getElementById("pass").value ){
+alert("Repeat Password Well");
+
+}
+  });
+
